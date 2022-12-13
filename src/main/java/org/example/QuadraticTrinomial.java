2cbb2aabffe98a3d73f84c0b1bf4addcd06f3ee4
Alexandre Merson <alexandrmerson35@gmail.com>
2022-12-13 11:32:08 +0600

    update

--- src/main/java/org/example/QuadraticTrinomial.java
+++ src/main/java/org/example/QuadraticTrinomial.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import static java.lang.Double.compare;
+
 public class QuadraticTrinomial extends Polynomial {
     public QuadraticTrinomial(double[] givenCoefficients) {
         super(givenCoefficients);
@@ -12,5 +14,46 @@ public class QuadraticTrinomial extends Polynomial {
 
     public double getDiscriminant() {
         return coefficients[1] * coefficients[1] - 4 * coefficients[2] * coefficients[0];
+    }
+
+    public PolynomialRoot[] solveQuadraticEquation() {
+        // initially we assume that Trinomial has got no roots
+        PolynomialRoot[] solution = new PolynomialRoot[] {
+            new PolynomialRoot(Double.NaN, Double.NaN )
+        };
+        PolynomialRoot x1;
+        PolynomialRoot x2;
+        final double discriminant = getDiscriminant();
+
+        if (compare(coefficients[2], 0.0) != 0) {
+            if (compare(discriminant, 0.0) >= 0) {
+                x1 = new PolynomialRoot (
+                    (-coefficients[ 1 ] - Math.sqrt(discriminant)) / (2 * coefficients[ 2 ]),
+                    0.0
+                );
+                x2 = new PolynomialRoot (
+                    (-coefficients[ 1 ] + Math.sqrt(discriminant)) / (2 * coefficients[ 2 ]),
+                    0.0
+                );
+            } else {
+                x1 = new PolynomialRoot (
+                    -coefficients[ 1 ] / (2 * coefficients[ 2 ]),
+                    Math.sqrt(-discriminant) / (2 * coefficients[ 2 ])
+                );
+                x2 = new PolynomialRoot (
+                    -coefficients[ 1 ] / (2 * coefficients[ 2 ]),
+                    -Math.sqrt(-discriminant) / (2 * coefficients[ 2 ])
+                );
+            }
+            solution = new PolynomialRoot[] { x1, x2 };
+        } else if (compare(coefficients[ 1 ], 0.0) != 0) {
+            solution = new PolynomialRoot[] {
+                new PolynomialRoot(-coefficients[ 0 ] / coefficients[ 1 ], 0.0)
+            };
+        } else if (compare(coefficients[ 0 ], 0.0) == 0) {
+            solution = new PolynomialRoot[] { };
+        }
+
+        return solution;
     }
 }
